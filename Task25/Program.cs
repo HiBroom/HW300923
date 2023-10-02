ca99a89d1d6d646a013ed0408df4e5a087d2c937
DzmitryMytsko <dzmitrymytsko@gmail.com>
2023-10-02 09:31:45 +0300

    Solved Task25 and Task27

---
+++ Task25/Program.cs
@@ -0,0 +1,44 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) 
+//  и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string readInput = Console.ReadLine();
+    int numbers = Convert.ToInt32(readInput);
+    return numbers; 
+}
+int Exponent(int numberA, int numberB)
+{
+    int result = 0;
+    for (int i = 1; i < numberB; i++)
+    {
+        result = numberA * numberA;
+        
+    }
+    return result;
+}
+bool TrueOrFalsExponent(int numberB)
+{
+    if(numberB < 0) return false;
+    else return true;
+}
+
+int a = Promt("Input number A: ");
+int b = Promt("Input number B: ");
+if (TrueOrFalsExponent(b))
+{
+    Console.WriteLine("Result - > Exponent(a, b)");
+}
+
+
+
+
+
+
+
+
+// int numberA = GetNumbers();
+// int numberB = GetNumbers();
---
+++ Task27/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int Promt(string message)
+{
+    Console.Write(message);
+    string readInput = Console.ReadLine();
+    int number = Convert.ToInt32(readInput);
+    return number;
+}
+
+int SumNumbers(int numbers)
+{
+    int sum = 0;
+    int devided = 0;
+    while(numbers != 0)
+    {
+        devided = numbers % 10;
+        sum = sum + devided;
+        numbers = numbers / 10;
+    }
+    return sum;
+
+}
+int input = Promt("Input number: ");
+
+Console.WriteLine($"Sum of numbers - > {SumNumbers(input)}");
